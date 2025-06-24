8ddd203720affc70478653e7e698f1c64677efae
Ryan Mar <Phyre205@gmail.com>
2025-06-24 15:23:41 -0700

    Update remtest.py

--- pyrois/remtest.py
+++ pyrois/remtest.py
@@ -1,11 +1,13 @@
 import hivemind
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
-dht = hivemind.DHT(
-    host_maddrs=['/ip4/0.0.0.0/tcp/8888'],
-    announce_maddrs=['/ip4/<your-public-ip>/tcp/8888'],
-    start=True
-)
+dht = hivemind.DHT(host_maddrs=['/ip4/0.0.0.0/tcp/8888'], start=True)
 
-input("DHT running. Press Enter to exit...\n")
+print("DHT is running. Press Ctrl+C to quit.")
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    print("Exiting...")
